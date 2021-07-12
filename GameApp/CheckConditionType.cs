45f56be2eaa11eec47e2a4038103021248e5a846
hemanth111p <72683647+hemanth111p@users.noreply.github.com>
2021-07-12 23:43:42 +0530

    updated

--- GameApp/CheckConditionType.cs
+++ GameApp/CheckConditionType.cs
@@ -8,29 +8,37 @@ namespace GameApp
 {
     class CheckConditionType
     {
-        BasicLevelReflection _basicLevel = new BasicLevelReflection();
-        IntermediateLevelReflection _intermediateLevel = new IntermediateLevelReflection();
-        AdvancedLevelReflection _advancedLevel = new AdvancedLevelReflection();
+        
+        LevelDeclaringType levelChoice = new LevelDeclaringType();
 
         public void CheckCondition(Options _choice)
         {
             switch (_choice)
             {
                 case Options.BASIC:
-                                    // basicLevel.BasicLevel();
-                    string path = @"C:\Users\hemanth.p\Documents\GitHub\latebinding-using-reflection-hemanth111p\GameApp\bin\Debug\LevelLibs\BasicLevelLib.dll";
-                    string type = "BasicLevelLib.BasicLevelType";
-                    string method = "Play";                   
-                    _basicLevel.BasicLevel(path, type, method);
+                    Console.WriteLine("Basic Level");
+                    string basicLevelPath = @"C:\Users\hemanth.p\Documents\GitHub\latebinding-using-reflection-hemanth111p\GameApp\bin\Debug\LevelLibs\BasicLevelLib.dll";
+                    string basicLevelType = "BasicLevelLib.BasicLevelType";
+                    string basicLevelMethod = "Play";     
+                    levelChoice.Level(basicLevelPath, basicLevelType, basicLevelMethod);
+                    
                     break;
 
                 case Options.INTERMEDIATE:
-                                   _intermediateLevel.IntermediateLevel();                    
+                    Console.WriteLine("Intermediate Level");
+                    string intermediateLevelPath = @"C:\Users\hemanth.p\Documents\GitHub\latebinding-using-reflection-hemanth111p\GameApp\bin\Debug\LevelLibs\IntermediateLevelLib.dll";
+                    string intermediateLevelType = "IntermediateLevelLib.IntermediateLevelType";
+                    string intermediateLevelMethod = "Start";
+                    levelChoice.Level(intermediateLevelPath, intermediateLevelType, intermediateLevelMethod);                    
                                    break;
 
                 case Options.ADVANCED:
-                                    _advancedLevel.AdvancedLevel();                   
-                                       break;
+                    Console.WriteLine("Advanced Level");
+                    string advancedLevelPath = @"C:\Users\hemanth.p\Documents\GitHub\latebinding-using-reflection-hemanth111p\GameApp\bin\Debug\LevelLibs\AdvancedLevelLib.dll";
+                    string advancedLevelType = "AdvancedLevelLib.AdvancedLevelType";
+                    string advancedLevelMethod = "Begin";
+                    levelChoice.Level(advancedLevelPath, advancedLevelType, advancedLevelMethod); ;
+                    break;
                     
 
             }
---
+++ GameApp/LevelDeclaringType.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameApp
+{
+    class LevelDeclaringType
+    {
+        public void Level(string path, string classType, string method)
+        {
+            
+            System.Reflection.Assembly levelLib =
+      System.Reflection.Assembly.LoadFile(path);
+            System.Type allLevelTypeClassRef = FindClass(levelLib, classType);
+            Object objRef = System.Activator.CreateInstance(allLevelTypeClassRef);
+            System.Reflection.MethodInfo _methodRef = allLevelTypeClassRef.GetMethod(method);
+            try
+            {
+                object result = InvokeMethod(objRef, _methodRef, new object[] { });
+                Console.WriteLine(result.ToString());
+            }
+
+            catch
+            {
+                try
+                {
+                    object result = InvokeMethod(objRef, _methodRef, new object[] {"hemanth" });
+                    Console.WriteLine(result.ToString());
+                }
+                catch
+                {
+                    object result = InvokeMethod(objRef, _methodRef, new object[] {"hemanth",10 });
+                    Console.WriteLine(result.ToString());
+                }
+            }
+
+
+        }
+        System.Type FindClass(System.Reflection.Assembly _assemblyRef, String name)
+        {
+            System.Type classType = _assemblyRef.GetType(name);
+            if (classType != null)
+            {
+                if (classType.IsClass)
+                {
+                    return classType;
+                }
+            }
+            return null;
+        }
+        object InvokeMethod(object source, System.Reflection.MethodInfo methodRef, object[] arguments)
+        {
+            return methodRef.Invoke(source, arguments);
+
+        }
+
+    }
+
+
+}
+   
+
+        
